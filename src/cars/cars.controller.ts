b72a932ea34b8665313de4a7b3b686e7c5f042c7
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-08-25 23:09:03 +0200

    feat(cars): finish create endpoint

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -31,7 +31,7 @@ export class CarsController {
 
   @Post()
   createCar(@Body() createCarDto: CreateCarDto) {
-    return createCarDto
+    return this.carsSerice.create(createCarDto)
   }
 
   @Patch(':id')
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { Car } from './interfaces/car.interface'
-import { v4 as uuid  } from 'uuid'
+import { v4 as uuid } from 'uuid'
+import { CreateCarDto } from './dto/create-car.dto'
 //nest g s cars --no-spec
 @Injectable()
 export class CarsService {
@@ -30,6 +31,12 @@ export class CarsService {
     const car = this.cars.find((c) => c.id === id)
     // Show Responses on Exception
     if (!car) throw new NotFoundException(`Car with id '${id}' not found`)
+    return car
+  }
+
+  create(createCarDto: CreateCarDto) {
+    const car: Car = { id: uuid(), ...createCarDto }
+    this.cars.push(car)
     return car
   }
 }
