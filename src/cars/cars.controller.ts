3a6681e01dbacac0830092714a7c7ec2e2fc4107
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-08-25 21:26:43 +0200

    feat(cars): add endpoint examples

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
 
 // nest g co cars
 // Actualiza su modulo correspondiente
 @Controller('cars')
 export class CarsController {
+  private cars = ['Toyota', 'Mercedes', 'Honda']
+
   @Get()
   getAllCars() {
-    return ['Toyota', 'Mercedes', 'Honda']
+    return this.cars
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id: number) {
+    console.log({ id })
+    return { car: this.cars[id] }
   }
 }
