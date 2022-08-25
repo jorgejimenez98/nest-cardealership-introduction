577e84b80bd5d0bf4d97b4d54eed1087ac4e7f48
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-08-26 00:06:26 +0200

    feat(brands): finish endpoints after generations

--- src/brands/brands.controller.ts
+++ src/brands/brands.controller.ts
@@ -6,11 +6,13 @@ import {
   Patch,
   Param,
   Delete,
+  ParseUUIDPipe,
 } from '@nestjs/common'
 import { BrandsService } from './brands.service'
 import { CreateBrandDto } from './dto/create-brand.dto'
 import { UpdateBrandDto } from './dto/update-brand.dto'
 
+// nest g resource brands
 @Controller('brands')
 export class BrandsController {
   constructor(private readonly brandsService: BrandsService) {}
@@ -26,17 +28,20 @@ export class BrandsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.brandsService.findOne(+id)
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.brandsService.findOne(id)
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBrandDto: UpdateBrandDto) {
-    return this.brandsService.update(+id, updateBrandDto)
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateBrandDto: UpdateBrandDto,
+  ) {
+    return this.brandsService.update(id, updateBrandDto)
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.brandsService.remove(+id)
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.brandsService.remove(id)
   }
 }
--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,26 +1,55 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { CreateBrandDto } from './dto/create-brand.dto'
 import { UpdateBrandDto } from './dto/update-brand.dto'
+import { Brand } from './entities/brand.entity'
+import { v4 as uuid } from 'uuid'
 
 @Injectable()
 export class BrandsService {
-  create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand'
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Brand 1',
+      createdAt: new Date().getTime(),
+    },
+  ]
+
+  create({ name }: CreateBrandDto) {
+    const brand: Brand = {
+      id: uuid(),
+      name: name.toLowerCase(),
+      createdAt: new Date().getTime(),
+    }
+    this.brands.push(brand)
+    return brand
   }
 
-  findAll() {
-    return `This action returns all brands`
+  findAll(): Brand[] {
+    return this.brands
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`
+  findOne(id: string): Brand {
+    const brand: Brand = this.brands.find((b: Brand) => b.id === id)
+    if (!brand) {
+      throw new NotFoundException(`Brand with id "${id}" not found `)
+    }
+    return brand
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brandDB: Brand = this.findOne(id)
+    this.brands = this.brands.map((brand: Brand) => {
+      if (brand.id === id) {
+        brandDB.updatedAt = new Date().getTime()
+        brandDB = { ...brandDB, ...updateBrandDto }
+        return brandDB
+      }
+      return brand
+    })
+    return brandDB
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} brand`
+  remove(id: string) {
+    this.brands = this.brands.filter((b: Brand) => b.id !== id)
   }
 }
